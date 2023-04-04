f0571d492d3adbe77ccf783bcda61e5cc9e240fa
jiazongbao <wb-jiazongbao@rokid.com>
2023-04-04 11:53:56 +0800

    feat: 优化首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -16,15 +16,11 @@
         </div>
       </template>
       <div class="head">共有<span>{{ devInfo.DevSummary.AllNum }}</span>台设备</div>
+      <!-- 饼图 -->
       <PieChart :chartData="devInfo" />
       <div class="head">农场地块</div>
-      <main class="allFarm">
-        <div class="allFarm_item" v-for="item in devInfo.DevSummary.DevType" :key="item.TypeId">
-          <img :src="item.Icon" alt="">
-          <span class="item-title">{{ item.TypeName }}</span>
-          <div class="item-text">运行<span>{{ item.OnlineNum }}</span>/总{{ item.AllNum }}</div>
-        </div>
-      </main>
+      <!-- 农场地块 -->
+      <FarmPlot :plotData="devInfo.DevSummary.DevType" />
     </van-pull-refresh>
   </div>
 </template>
@@ -36,6 +32,7 @@ import { userStore } from '@/store/user';
 import { devSummary } from '@/api/home'
 import { DevSummaryItem } from './index'
 import PieChart from './pieChart.vue'
+import FarmPlot from './farmPlot.vue'
 
 const store = userStore();
 const devInfo = reactive<DevSummaryItem>({
@@ -100,49 +97,6 @@ onMounted(() => {
 
     span {
       color: #00CC90;
-    }
-  }
-
-
-  .allFarm {
-    display: flex;
-    flex-wrap: wrap;
-
-    .allFarm_item {
-      box-sizing: border-box;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      margin-bottom: 14px;
-      width: 104px;
-      height: 108px;
-      border-radius: 4px;
-      border: 1px solid #333333;
-
-      img {
-        margin-top: 12px;
-        width: 40px;
-        height: 40px;
-      }
-
-      .item-title {
-        margin: 5px 0 2px;
-        font-size: 14px;
-        color: #CCCCCC;
-      }
-
-      .item-text {
-        font-size: 12px;
-        color: #d2d3d4;
-      }
-
-      span {
-        color: #00CC90;
-      }
-    }
-
-    .allFarm_item:nth-child(3n-1) {
-      margin: 0 14px;
     }
   }
 }
