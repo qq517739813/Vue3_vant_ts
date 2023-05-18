857ab42cab5488882214900f61101b92098d9286
tss001 <517739813@qq.com>
2023-05-18 16:13:38 +0800

    fix: 尝试修改视频切换

--- src/views/field/detail/videoPlayer.vue
+++ src/views/field/detail/videoPlayer.vue
@@ -1,11 +1,7 @@
 <template>
   <div :style="videoWrapStyles">
-    <video
-      v-if="props.src"
-      id="videoMain"
-      ref="videoRef"
-      class="video-main video-js vjs-default-skin vjs-big-play-centered"
-    >
+    <video v-if="props.src" id="videoMain" ref="videoRef"
+      class="video-main video-js vjs-default-skin vjs-big-play-centered">
       <source :src="props.src" type="application/x-mpegURL" />
     </video>
   </div>
@@ -46,6 +42,9 @@ watch(myPropRef, (newVal, oldVal) => {
   console.log('newVal', newVal);
   console.log('oldVal', oldVal);
   if (srcPath.value !== newVal) {
+    if (player.value) {
+      player.value.dispose();
+    }
     srcPath.value = newVal;
     initVideo();
   }
@@ -53,27 +52,31 @@ watch(myPropRef, (newVal, oldVal) => {
 // 初始化videojs
 const initVideo = () => {
   nextTick(() => {
-  const options = {
-    // language: 'zh-CN', // 设置语言
-    autoplay: true, // 设置自动播放
-    controls: false, // 是否显示底部控制栏
-    techOrder: ['html5',],
-    preload: 'auto', // 预加载
-    fluid: false, // 自适应宽高
-    muted: true, // 设置了它为true，才可实现自动播放,同时视频也被静音
-    // src// 要嵌入的视频源的源 URL
-  };
-  if (videoRef.value) {
-    // 创建 video 实例
-    player.value = videojs(videoRef.value, options);
-  }
+    const options = {
+      // language: 'zh-CN', // 设置语言
+      autoplay: true, // 设置自动播放
+      controls: false, // 是否显示底部控制栏
+      techOrder: ['html5',],
+      preload: 'auto', // 预加载
+      fluid: false, // 自适应宽高
+      muted: true, // 设置了它为true，才可实现自动播放,同时视频也被静音
+      // src// 要嵌入的视频源的源 URL
+    };
+    if (videoRef.value) {
+      // 创建 video 实例
+      player.value = videojs(videoRef.value, videojs.obj.merge(options));   
+
+      // 创建 video 实例
+      // player.value = videojs(videoRef.value, videojs.obj.mergeOptions(options));
+
+    }
   });
 };
 
 onMounted(() => {
-//   setTimeout(() => {
-    initVideo();
-//   }, 500);
+  //   setTimeout(() => {
+  initVideo();
+  //   }, 500);
 });
 onUnmounted(() => {
   if (player.value) {
